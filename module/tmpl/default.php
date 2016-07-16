519af79b62a9c704965ef45247e82550330a5571
Yves Hoppe <yves@compojoom.com>
2016-07-16 20:06:34 +0200

    cs

--- module/tmpl/default.php
+++ module/tmpl/default.php
@@ -11,9 +11,9 @@
 defined('_JEXEC') or die;
 
 // Access to module parameters
-$url = $params->get('url', 'https://www.joomla.org');
+$domain = $params->get('domain', 'https://www.joomla.org');
 ?>
 
-<a href="<?php echo $url; ?>">
+<a href="<?php echo $domain; ?>">
 	<?php echo '[PROJECT_NAME]'; ?>
 </a>
