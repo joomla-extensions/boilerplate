3b93f6dff59849f3166fb2044d316cad297b6d0e
anibalsanchez <anibal.sanchez@extly.com>
2018-05-10 15:35:11 +0200

    0.0.1

---
+++ file/foo_cli.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package     Extly.Apps
+ * @subpackage  com_joocial - The future hub for content management systems.
+ *
+ * @author      Extly, CB. <team@extly.com>
+ * @copyright   Copyright (c)2007-2018 Extly, CB. All rights reserved.
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ * @link        https://www.extly.com
+ *
+ */
+
+defined('_JEXEC') or die;
