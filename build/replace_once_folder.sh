8de105b8b3983daa790096a3690bd16ecc5b8051
anibalsanchez <anibal.sanchez@extly.com>
2018-05-22 11:55:13 +0200

    handy bash script

--- build/replace_once_folder.sh
+++ build/replace_once_folder.sh
@@ -2,15 +2,17 @@
 
 # A handy bash script to be execute only once on the extension-specific directories
 
-find $1 -type f -exec sed -i 's/\[EXTENSION_NAME\]/JG Auth/g' {} \;
-find $1 -type f -exec sed -i 's/\[AUTHOR\]/Extly, CB/g' {} \;
-find $1 -type f -exec sed -i 's/\[AUTHOR_EMAIL\]/team@extly.com/g' {} \;
-find $1 -type f -exec sed -i 's/\[COPYRIGHT\]/Copyright (c)2007-2018 Extly, CB All rights reserved./g' {} \;
-find $1 -type f -exec sed -i 's/\[LICENSE\]/GNU General Public License version 3 or later; see LICENSE.txt/g' {} \;
-find $1 -type f -exec sed -i 's/\[AUTHOR_URL\]/https\:\/\/www\.extly\.com/g' {} \;
+find $1 -type f -not -name "*.sh" -exec sed -i 's/\[EXTENSION_NAME\]/JG Auth/g' {} \;
+find $1 -type f -not -name "*.sh" -exec sed -i 's/\[PACKAGE_NAME\]/JG Auth/g' {} \;
+find $1 -type f -not -name "*.sh" -exec sed -i 's/\[PROJECT_NAME]/JG Auth/g' {} \;
+find $1 -type f -not -name "*.sh" -exec sed -i 's/\[AUTHOR\]/Extly, CB/g' {} \;
+find $1 -type f -not -name "*.sh" -exec sed -i 's/\[AUTHOR_EMAIL\]/team@extly.com/g' {} \;
+find $1 -type f -not -name "*.sh" -exec sed -i 's/\[COPYRIGHT\]/Copyright (c)2007-2018 Extly, CB All rights reserved./g' {} \;
+find $1 -type f -not -name "*.sh" -exec sed -i 's/\[LICENSE\]/GNU General Public License version 3 or later; see LICENSE.txt/g' {} \;
+find $1 -type f -not -name "*.sh" -exec sed -i 's/\[AUTHOR_URL\]/https\:\/\/www\.extly\.com/g' {} \;
 
-find $1 -type f -exec sed -i 's/com_jgauth/com_jgauth/g' {} \;
-find $1 -type f -exec sed -i 's/COM_JGAUTH/COM_JGAUTH/g' {} \;
+find $1 -type f -not -name "*.sh" -exec sed -i 's/com_jgauth/com_jgauth/g' {} \;
+find $1 -type f -not -name "*.sh" -exec sed -i 's/COM_FOO/COM_JGAUTH/g' {} \;
 
 find $1 -name "*.php" -type f -exec sed -i 's/class Foo/class JgAuth/g' {} \;
 find $1 -name "*.php" -type f -exec sed -i 's/ \* Foo/ \* JgAuth/g' {} \;
